8033d9527cf9e34df7fb449fd2e38a5ccec7608e
Zoe Geauthreaux <Zoe Geauthreaux>
2016-08-30 09:04:35 -0500

    Modified Sketch

--- sketch.js
+++ sketch.js
@@ -1,4 +1,11 @@
 function setup() {
 createCanvas(500,500);
-  ellipse(100, 100, 50);
+  ellipse(25, 25, 100);
+  ellipse(95, 95, 100);
+  ellipse(25, 165, 100);
+  ellipse(-45, 95, 100);
+  ellipse(25, 290, 150);
+  ellipse(130, 200, 150);
+  ellipse(200, 300, 200);
+  ellipse(250, 200, 250);
 }
